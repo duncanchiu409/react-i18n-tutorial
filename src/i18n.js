c7db73c5174dd68e84ba9c8fca6408006b0398f1
fG40912 <futureg412@gmail.com>
2024-03-31 18:55:31 +0800

    'i8next-http-backend' library added

---
+++ src/i18n.js
@@ -0,0 +1,15 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector"
+import Backend from 'i18next-http-backend'
+
+i18n.use(LanguageDetector).use(Backend).use(initReactI18next).init(
+    {
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+          escapeValue: false, // not needed for react as it escapes by default
+        },
+    });
+
+export default i18n
